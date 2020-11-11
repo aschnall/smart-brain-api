162daede5462820472b787779a7d90ad608526f3
Aaron Schnall <aschnall@Aarons-Air.fios-router.home>
2020-11-11 13:31:33 -0500

    changing response

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) => {res.send('it is working')})
+
 //signin
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
