732a15d54d16106534d6535eed2552a29bc7ce0f
Aaron Schnall <aschnall@Aarons-Air.fios-router.home>
2020-11-11 15:52:58 -0500

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
   connection: {
-    host : 'postgresql-amorphous-81957',
-    user : 'aschnall',
-    password : '',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
